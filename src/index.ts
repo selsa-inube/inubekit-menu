a15492dd2b2e2b3fb3badeb366b05204bccabfc1
cmarin001 <cmarin6051@gmail.com>
2024-08-20 19:31:21 -0500

    feat: added menu-component and its dependencies

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-export * from ".";
+export { Menu } from "./Menu";
+export { MenuUser } from "./Menu/MenuUser";
+export { MenuSection } from "./Menu/MenuSection";
+export { MenuItem } from "./Menu/MenuItem";
+
+export type { IMenu } from "./Menu";
+export type { IMenuUser } from "./Menu/MenuUser";
+export type { IMenuSection } from "./Menu/MenuSection";
+export type { IMenuItem } from "./Menu/MenuItem";
+export type { MenuItemSpacingType } from "./Menu/MenuItem/props";
+export type { ISection } from "./Menu/MenuSection/props";
