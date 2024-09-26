54d235741178b35fdf8c250315c9ccb0f8598e93
cmarin001 <cmarin6051@gmail.com>
2024-09-26 10:34:01 -0500

    chore: move menu tokens from foundations to component

--- src/Menu/MenuItem/styles.js
+++ src/Menu/MenuItem/styles.js
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 import { Link } from "react-router-dom";
-import { inube } from "@inubekit/foundations";
+import { tokens } from "../../Menu/Tokens/tokens";
 
 const StyledMenuItemContainer = styled(Link)`
   display: flex;
@@ -12,12 +12,12 @@ const StyledMenuItemContainer = styled(Link)`
   background-color: ${({ $disabled, theme }) =>
     $disabled &&
     (theme?.menu?.item?.background?.disabled ||
-      inube.menu.item.background.disabled)};
+      tokens.item.background.disabled)};
 
   &:hover {
     cursor: ${({ $disabled }) => ($disabled ? "not-allowed" : "pointer")};
     background-color: ${({ theme }) =>
-      theme?.menu?.item?.background?.hover || inube.menu.item.background.hover};
+      theme?.menu?.item?.background?.hover || tokens.item.background.hover};
   }
 `;
 
