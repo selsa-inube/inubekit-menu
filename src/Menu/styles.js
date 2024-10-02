cce5c2c0b62f1cf0c9746188c39a4c2c4cff39c6
cmarin001 <cmarin6051@gmail.com>
2024-10-02 15:18:46 -0500

    fix: adjust box-shadows for menu

--- src/Menu/styles.js
+++ src/Menu/styles.js
@@ -1,12 +1,17 @@
 import { tokens } from "./Tokens/tokens";
 import styled from "styled-components";
+import { inube } from "@inubekit/foundations";
 
 const StyledMenuContainer = styled.div`
   overflow: hidden;
   border-radius: 8px;
   width: 312px;
-  box-shadow: 0px 2px 3px 0px #091e4221;
-  box-shadow: 0px 6px 10px 4px #091e4221;
+  box-shadow: 0px 2px 3px 0px
+    ${({ theme }) =>
+      theme?.palette?.neutralAlpha?.N40A || inube.palette.neutralAlpha.N40A};
+  box-shadow: 0px 6px 10px 4px
+    ${({ theme }) =>
+      theme?.palette?.neutralAlpha?.N40A || inube.palette.neutralAlpha.N40A};
   background-color: ${({ theme }) =>
     theme?.menu?.background?.color || tokens.background.color};
 `;
