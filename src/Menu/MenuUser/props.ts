802a9cf8b7ac2618a89084814f508b5cf3d25970
Fernando Cañas <fernando.canas@outlook.com>
2024-10-23 14:39:55 -0500

    feat(MenuUser): refactor styles and add gap and padding props

--- src/Menu/MenuUser/props.ts
+++ src/Menu/MenuUser/props.ts
@@ -2,35 +2,7 @@ const menuItemSpacing = ["wide", "compact"] as const;
 
 type MenuItemSpacingType = (typeof menuItemSpacing)[number];
 
-const props = {
-  title: {
-    description: "A string that represents the title of the menu item.",
-  },
-  path: {
-    description:
-      "Corresponds to the path of the menu item where the user will be redirected",
-  },
-  description: {
-    description:
-      "An optional string that provides additional information about the menu item.",
-  },
-  spacing: {
-    description:
-      "An optional value of type `MenuItemSpacingType` that controls the spacing around the menu item. The default value is `wide`.",
-  },
-  iconBefore: {
-    description:
-      "An optional JSX element that represents an icon to be displayed before the title of the menu item.",
-  },
-  iconAfter: {
-    description:
-      "An optional JSX element that represents an icon to be displayed after the title of the menu item.",
-  },
-  disabled: {
-    description:
-      "An optional boolean that indicates whether the menu item is currently disabled.",
-  },
-};
+const props = {};
 
 export { menuItemSpacing, props };
 export type { MenuItemSpacingType };
